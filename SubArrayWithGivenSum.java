a2881557411b2ef6486dada14b66a765ebcef270
Shivam Singhal <swingle8@gmail.com>
2020-07-17 21:14:10 +0530

    Update SubArrayWithGivenSum.java

--- SubArrayWithGivenSum.java
+++ SubArrayWithGivenSum.java
@@ -1,61 +1,61 @@
 //Sub-array with given sum
-Sub-array with given sum
-Problem Description
+// Sub-array with given sum
+// Problem Description
 
-Given an array of positive integers A and an integer B, find and return first continuous subarray which adds to B.
+// Given an array of positive integers A and an integer B, find and return first continuous subarray which adds to B.
 
-If the answer does not exist return an array with a single element "-1".
+// If the answer does not exist return an array with a single element "-1".
 
-First sub-array means the sub-array for which starting index in minimum.
-
-
-
-Problem Constraints
-1 <= length of the array <= 100000
-1 <= A[i] <= 109
-1 <= B <= 109
+// First sub-array means the sub-array for which starting index in minimum.
 
 
 
-Input Format
-The first argument given is the integer array A.
-
-The second argument given is integer B.
-
-
-
-Output Format
-Return the first continuous sub-array which adds to B and if the answer does not exist return an array with a single element "-1".
+// Problem Constraints
+// 1 <= length of the array <= 100000
+// 1 <= A[i] <= 109
+// 1 <= B <= 109
 
 
 
-Example Input
-Input 1:
+// Input Format
+// The first argument given is the integer array A.
 
- A = [1, 2, 3, 4, 5]
- B = 5
-Input 2:
-
- A = [5, 10, 20, 100, 105]
- B = 110
+// The second argument given is integer B.
 
 
-Example Output
-Output 1:
 
- [2, 3]
-Output 2:
-
- -1
+// Output Format
+// Return the first continuous sub-array which adds to B and if the answer does not exist return an array with a single element "-1".
 
 
-Example Explanation
-Explanation 1:
 
- [2, 3] sums up to 5.
-Explanation 2:
+// Example Input
+// Input 1:
 
- No subarray sums up to required number.
+//  A = [1, 2, 3, 4, 5]
+//  B = 5
+// Input 2:
+
+//  A = [5, 10, 20, 100, 105]
+//  B = 110
+
+
+// Example Output
+// Output 1:
+
+//  [2, 3]
+// Output 2:
+
+//  -1
+
+
+// Example Explanation
+// Explanation 1:
+
+//  [2, 3] sums up to 5.
+// Explanation 2:
+
+//  No subarray sums up to required number.
 
 
 
